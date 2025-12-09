a6ab8824690f687c8157f0a3e5bdcacafb69f456
Adam Saudagar <adsau59@gmail.com>
2025-12-09 17:50:41 +0530

    updated the script to append local config which we dont want ot share on github

--- install/config/config.sh
+++ install/config/config.sh
@@ -1,6 +1,27 @@
 # Copy over Omarchy configs
 mkdir -p ~/.config
-cp -R ~/.local/share/omarchy/config/* ~/.config/
+
+#!/usr/bin/env bash
+
+SRC="$HOME/.local/share/omarchy/config"
+DST="$HOME/.config"
+
+# Copy all non-.local files first
+rsync -a --exclude='*.local' "$SRC/" "$DST/"
+
+# Append/create from .local files
+find "$SRC" -type f -name "*.local" | while read -r local_file; do
+    base_file="${local_file%.local}"
+    relative="${base_file#$SRC/}"
+    target="$DST/$relative"
+
+    mkdir -p "$(dirname "$target")"
+    [ -f "$target" ] || touch "$target"
+
+    cat "$local_file" >> "$target"
+done
+
+
 
 # Use default bashrc from Omarchy
 cp ~/.local/share/omarchy/default/bashrc ~/.bashrc
